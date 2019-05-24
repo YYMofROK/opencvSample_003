6bff280385c12c5cb09a279729a4e95210ba2fa2
dev-yym <dev-yym@LAPTOP-J4U0QGE6>
2019-05-24 18:22:35 +0900

    1

---
+++ opencv_003/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace opencv_003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.log_1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(1147, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(1147, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // log_1
+            // 
+            this.log_1.Location = new System.Drawing.Point(646, 0);
+            this.log_1.Multiline = true;
+            this.log_1.Name = "log_1";
+            this.log_1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log_1.Size = new System.Drawing.Size(495, 480);
+            this.log_1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(251, 550);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 56);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "얼굴데이타수집";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(16, 580);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 550);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "식별값(성명)";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button4.Location = new System.Drawing.Point(396, 550);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 56);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "학습데이터 생성";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 776);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.log_1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox log_1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ opencv_003/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+
+
+
+
+
+
+using System.IO;
+using OpenCvSharp.XFeatures2D;
+
+namespace opencv_003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCapture videoCapture;
+        OpenCvSharp.Face.FisherFaceRecognizer model1;
+        OpenCvSharp.Face.EigenFaceRecognizer model2;
+
+        List<ImageInfo> Face_images_info = new List<ImageInfo>();
+
+
+
+
+
+        internal class ImageInfo
+        {
+            public Mat Image { set; get; }
+            public int ImageGroupId { set; get; }
+            public int ImageId { set; get; }
+            public String person_name { set; get; }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int deviceNum = 0; 99999 > deviceNum; deviceNum++)
+            {
+                videoCapture = new VideoCapture(deviceNum);
+                if (videoCapture.IsOpened() == true)
+                {
+                    log_write("카메라 연결 성공 DeviceID : " + Convert.ToString(deviceNum));
+                    break;
+                }
+            }
+
+            videoCapture.Set(CaptureProperty.FrameWidth, 640);
+            videoCapture.Set(CaptureProperty.FrameHeight, 480);
+
+            log_write("videoCapture.FrameWidth:" + Convert.ToString(videoCapture.FrameWidth));
+            log_write("videoCapture.FrameHeight:" + Convert.ToString(videoCapture.FrameHeight));
+
+            FaceTrain_Step2();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (videoCapture.IsOpened() == false)
+            {
+                log_write("카메라 연결 안됨");
+                timer1.Stop();
+            }
+
+
+            Mat frame_img_source_01 = new Mat();
+            videoCapture.Read(frame_img_source_01);
+
+
+            Mat searchFace = new Mat();
+            Mat temp_image = new Mat();
+
+            Rect[] faces = FaceDetect(frame_img_source_01);
+
+            for (int faceCnt = 0; faceCnt < faces.Length; faceCnt++)
+            {
+
+                //  log_write("faces:" + Convert.ToString(faces[faceCnt]));
+                if (faces[faceCnt].Width > 100)
+                {
+                    Cv2.Resize(frame_img_source_01[faces[faceCnt]], searchFace, new OpenCvSharp.Size(160, 160));
+                    byte[] imageBytes = searchFace.ToBytes(".bmp");
+                    searchFace = Mat.FromImageData(imageBytes, ImreadModes.Grayscale);
+
+                    var predictedGroupId = 0;
+                    if (model1 != null)
+                    {
+                        predictedGroupId = model1.Predict(searchFace);
+                    }
+
+                    if (predictedGroupId == 0)
+                    {
+                        log_write("predictedGroupId:" + Convert.ToString("확인불가"));
+                        log_write("predictedGroupId:" + Convert.ToString(predictedGroupId));
+                        Cv2.DestroyAllWindows();
+                    }
+                    else
+                    {
+                        log_write("predictedGroupId:" + Convert.ToString(predictedGroupId));
+                        log_write("성명:" + Convert.ToString(Face_images_info.FirstOrDefault(w => w.ImageGroupId == predictedGroupId).person_name));
+
+                        image_maatching(searchFace, Face_images_info.FirstOrDefault(w => w.ImageGroupId == predictedGroupId).Image);
+                        
+                    }
+
+                    Cv2.Rectangle(frame_img_source_01, faces[faceCnt], Scalar.YellowGreen, 2);
+                }
+                
+            }
+
+            Cv2.Flip(frame_img_source_01, frame_img_source_01, FlipMode.Y);
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame_img_source_01);
+
+        }
+
+
+        private void image_maatching(Mat img1, Mat img2)
+        {
+
+            Cv2.ImShow("Matches1", img1);
+            Cv2.ImShow("Matches2", img2);
+            
+            var detector = SURF.Create(hessianThreshold: 300,4,2,true,false); //A good default value could be from 300 to 500, depending from the image contrast.
+
+            KeyPoint[] keypoints1 = null;
+            KeyPoint[] keypoints2 = null;
+
+            Mat descriptors1 = new Mat();
+            Mat descriptors2 = new Mat();
+
+            detector.DetectAndCompute(img1, null, out keypoints1, descriptors1);
+            detector.DetectAndCompute(img2, null, out keypoints2, descriptors2);
+
+            var matcher = new BFMatcher();
+            var matches = matcher.Match(descriptors1, descriptors2);
+
+            float max_dist = 50;
+            int cntSuccessPoint = 0;
+            for (int i = 0; i < matches.Length; i++)
+            {
+                log_write("matches[i].Distance:" + Convert.ToString(max_dist) +"--"+ Convert.ToString(matches[i].Distance));
+                if(( matches[i].Distance * 100 ) < max_dist)
+                {
+                    cntSuccessPoint = cntSuccessPoint + 1;
+                }
+            }// end for
+
+            double rate = (cntSuccessPoint * 100) / matches.Length;
+            log_write("유사율:" + Convert.ToString(rate)  +"---"+ Convert.ToString(cntSuccessPoint) + "/" + Convert.ToString(matches.Length) );
+
+            var imgMatches = new Mat();
+            Cv2.DrawMatches(img1, keypoints1, img2, keypoints2, matches, imgMatches);
+            Cv2.ImShow("Matches3", imgMatches);
+
+        }
+
+
+
+        private void FaceTrain_Step1(Mat source_image, Rect[] faces)
+        {
+            timer1.Stop();
+            
+            byte[] imageBytes = source_image.ToBytes(".bmp");
+            source_image = Mat.FromImageData(imageBytes, ImreadModes.Grayscale);
+
+            Mat temp_image = new Mat();
+
+            String saveBitmapPath = null;
+            String DirName = textBox1.Text;
+            Bitmap tempBitMap = null;
+
+            OpenCvSharp.Size size = new OpenCvSharp.Size(30,30);
+
+            for (int faceCnt = 0; faceCnt < faces.Length; faceCnt++)
+            {
+                Cv2.Resize(source_image[faces[faceCnt]], temp_image, new OpenCvSharp.Size(160, 160));
+
+                tempBitMap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(temp_image);
+                pictureBox1.Image = tempBitMap;
+                log_write("faceCnt_images.Add:" + Convert.ToString(faceCnt));
+
+                saveBitmapPath = "C://Users//dev-yym//source//repos//opencv_003//Face_image";
+                saveBitmapPath = saveBitmapPath + "//" + Convert.ToString(DirName) + "//";
+
+                DirectoryInfo di = new DirectoryInfo(saveBitmapPath);
+
+                if(di.Exists == false)
+                {
+                    di.Create();
+                }
+
+                saveBitmapPath = saveBitmapPath + "//";
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Year);
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Month);
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Day);
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Hour);
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Minute);
+                saveBitmapPath = saveBitmapPath + Convert.ToString(DateTime.Now.Second);
+                saveBitmapPath = saveBitmapPath + "_" + Convert.ToString(faceCnt) + ".bmp";
+                log_write("saveBitmapPath:" + Convert.ToString(saveBitmapPath));
+
+                tempBitMap.Save(saveBitmapPath);
+            }
+
+
+            timer1.Start();
+        }
+
+
+
+        private void FaceTrain_Step2()
+        {
+            model1 = null;
+            model2 = null;
+            Face_images_info.Clear();
+
+            var imageId = 0;
+            var groupId = 0;
+            String saveBitmapPath = "C://Users//dev-yym//source//repos//opencv_003//Face_image";
+
+            foreach (var dir in new DirectoryInfo(saveBitmapPath).GetDirectories())
+            {
+                
+                foreach (var imageFile in dir.GetFiles("*.bmp"))
+                {
+                    Face_images_info.Add(new ImageInfo
+                    {
+                        Image = new Mat(imageFile.FullName,  ImreadModes.Grayscale),
+                        ImageId = imageId++,
+                        ImageGroupId = groupId++,
+                        person_name = dir.Name
+                    });
+                }
+
+            }
+
+            
+
+            if ( imageId > 0 )
+            {
+                
+                model1 = OpenCvSharp.Face.FisherFaceRecognizer.Create();
+                model1.Train(Face_images_info.Select(x => x.Image), Face_images_info.Select(x => x.ImageGroupId));
+                log_write("OpenCvSharp.Face.FisherFaceRecognizer - 학습");
+
+                
+                model2 = OpenCvSharp.Face.EigenFaceRecognizer.Create();
+                model2.Train(Face_images_info.Select(x => x.Image), Face_images_info.Select(x => x.ImageGroupId));
+                log_write("OpenCvSharp.Face.EigenFaceRecognizer - 학습");
+
+
+
+
+            }
+            //:Mat[1 * 20 * CV_32SC1, IsContinuous = True, IsSubmatrix = False, Ptr = 0x169dddf0, Data = 0x16a5e640]
+
+
+        }
+
+
+
+
+        private Rect[] FaceDetect(Mat source_image)
+        {   
+
+            // 그레이 스케일
+            Mat frame_img_gray = new Mat();
+            byte[] imageBytes = source_image.ToBytes(".bmp");
+            //byte[] imageBytes = frame_img_FlipY.ToBytes(".bmp");
+            frame_img_gray = Mat.FromImageData(imageBytes, ImreadModes.Grayscale);
+
+            Cv2.EqualizeHist(frame_img_gray, frame_img_gray);
+            var cascade = new CascadeClassifier("C://Users//dev-yym//source//repos//opencv_003//FaceML_Data//haarcascade_frontalface_alt.xml");
+            //var nestedCascade = new CascadeClassifier("C://Users//dev-yym//source//repos//opencv_002//FaceML_Data//haarcascade_eye_tree_eyeglasses.xml");
+
+            var faces = cascade.DetectMultiScale(
+                image: frame_img_gray,
+                scaleFactor: 1.1,
+                minNeighbors: 2,
+                flags: HaarDetectionType.DoRoughSearch | HaarDetectionType.ScaleImage,
+                minSize: new OpenCvSharp.Size(30, 30)
+                );
+            return faces;
+        }
+
+
+
+        private void log_write(String logText)
+        {
+            log_1.Text = Convert.ToString(DateTime.Now) + "-" + logText + "\r\n" + log_1.Text;
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            log_1.Text = null;
+            log_write("START BUTTON CLICK");
+
+            timer1.Stop();
+            timer1.Interval = 50; //스케쥴 간격 설정
+            timer1.Start(); //타이머를 가동시킨다.
+
+            Cv2.DestroyAllWindows();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            log_write("STOP BUTTON CLICK");
+            timer1.Stop();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+            Mat source_image = new Mat();
+            videoCapture.Read(source_image);
+            Rect[] faces = FaceDetect(source_image);
+
+            FaceTrain_Step1(source_image, faces);
+
+
+            
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            FaceTrain_Step2();
+        }
+    }
+}
+
+
+
+
+            
+
+
+
+            
+
+
+            
